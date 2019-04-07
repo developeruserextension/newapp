6d4432e75897c864869b1f6410ad880958e84835
amitsaini14424 <40263639+amitsaini14424@users.noreply.github.com>
2019-04-07 23:09:01 +0530

    update after routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { AboutUsComponent } from './about-us/about-us.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePageComponent
+  },
+  {
+    path: 'about',
+    component: AboutUsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
